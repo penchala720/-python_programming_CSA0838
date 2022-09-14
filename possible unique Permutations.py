f9b044990689b0b9a19aa5a7698ee567365ea8a1
penchala720 <112766489+penchala720@users.noreply.github.com>
2022-09-14 09:49:10 +0530

    Add files via upload

---
+++ possible unique Permutations.py
@@ -0,0 +1,23 @@
+def permutFunc(myList):
+
+
+	if len(myList) == 0:
+		return []
+
+	if len(myList) == 1:
+		return [myList]
+
+
+	k = []
+
+	for i in range(len(myList)):
+		m = myList[i]
+		res = myList[:i] + myList[i+1:]
+		for p in permutFunc(res):
+			k.append([m] + p)
+	return k
+
+
+myList = list([1,1,1])
+for p in permutFunc(myList):
+	print (p)
